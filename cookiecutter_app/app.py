319281aa4bd08c0faa2f88eb9a118dc75e70d2fc
goddessana <twicegoddessana1229@gmail.com>
2024-04-20 19:47:36 +0900

    refactor: fix settings to class-based

--- cookiecutter_app/app.py
+++ cookiecutter_app/app.py
@@ -1,7 +1,7 @@
-from cookiecutter_app import settings
+from cookiecutter_app.settings import ProductionConfig
 from cookiecutter_app.app_factory import create_app
 
-app = create_app(config_object=settings)
+app = create_app(config_object=ProductionConfig)
 
 if __name__ == "__main__":
     app.run()
