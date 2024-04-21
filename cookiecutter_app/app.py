48d33588a0f4b390b7219d5b27f8e64b525fa080
goddessana <twicegoddessana1229@gmail.com>
2024-04-21 20:32:51 +0900

    feat: add options to select flask config objects from .env

--- cookiecutter_app/app.py
+++ cookiecutter_app/app.py
@@ -1,7 +1,12 @@
 from cookiecutter_app.settings import ProductionConfig
 from cookiecutter_app.app_factory import create_app
 
-app = create_app(config_object=ProductionConfig)
+from environs import Env
+
+env = Env()
+env.read_env()
+
+app = create_app(config_object=env.str("APP_CONFIG_OBJECT", ProductionConfig))
 
 if __name__ == "__main__":
     app.run()
