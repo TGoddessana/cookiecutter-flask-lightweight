b04b5fd4bc65564d55de8d3edaa0e596c220bd79
goddessana <twicegoddessana1229@gmail.com>
2024-04-21 19:14:12 +0900

    teat: add count assertions

--- cookiecutter_app/tests/users/test_models.py
+++ cookiecutter_app/tests/users/test_models.py
@@ -7,6 +7,7 @@ def test_user_creation(db) -> None:
     user = User.create(username='test', email='test@test.com', password='test123')
     db.session.commit()
 
+    assert User.query.count() == 1
     assert user.username == 'test'
     assert user.email == 'test@test.com'
     assert user.check_password('test123')
@@ -19,6 +20,8 @@ def test_username_validation(db) -> None:
     with pytest.raises(ValueError):
         User.create(username='test', email='test2@test.com', password='test123')
 
+    assert User.query.count() == 1
+
 
 def test_email_validation(db) -> None:
     User.create(username='test', email='test@test.com', password='test123')
@@ -27,11 +30,14 @@ def test_email_validation(db) -> None:
     with pytest.raises(ValueError):
         User.create(username='test2', email='invalid_email', password='test123')
 
+    assert User.query.count() == 1
+
 
 def test_password_hashed(db) -> None:
     user = User.create(username='test', email="test@test.com", password="test123")
     db.session.commit()
 
+    assert User.query.count() == 1
     assert user.password is not "test123"
 
 
@@ -39,5 +45,6 @@ def test_password_check(db) -> None:
     user = User.create(username='test', email='test@test.com', password='test123')
     db.session.commit()
 
+    assert User.query.count() == 1
     assert user.check_password('test123')
     assert not user.check_password('wrong_password')
